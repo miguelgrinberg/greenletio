2e7be4b8266644733c3764a6f8e8e03e46a68479
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-07-17 18:04:27 +0100

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="greenletio",
-    version="0.0.1",
+    version="0.0.2",
     author="Miguel Grinberg",
     author_email="miguel.grinberg@gmail.com",
     description="Asyncio integration with sync code using greenlets.",
