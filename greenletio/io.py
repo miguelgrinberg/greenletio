de8466bf6d5d47cd97ad90ea312cb839f64188e1
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-07-25 16:53:33 +0100

    i/o waiting support

---
+++ greenletio/io.py
@@ -0,0 +1,26 @@
+from greenlet import getcurrent
+from greenletio.core import bridge
+
+
+def _reader_callback(fd, gl):
+    bridge.loop.remove_reader(fd)
+    bridge.schedule(gl)
+
+
+def _writer_callback(fd, gl):
+    bridge.loop.remove_writer(fd)
+    bridge.schedule(gl)
+
+
+def wait_to_read(fd):
+    if not bridge.running and not bridge.starting:
+        bridge.start()
+    bridge.loop.add_reader(fd, _reader_callback, fd, getcurrent())
+    bridge.switch()
+
+
+def wait_to_write(fd):
+    if not bridge.running and not bridge.starting:
+        bridge.start()
+    bridge.loop.add_writer(fd, _writer_callback, fd, getcurrent())
+    bridge.switch()
