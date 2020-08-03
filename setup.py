5625f33d557c3fc99f3402261d335de701133435
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-08-03 13:54:33 +0100

    fix setup to include green sub-package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/miguelgrinberg/greenletio",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['tests']),
     install_requires=[
         'greenlet'
     ],
